9420e4c8821d42ff6536dc173c04a7261ebc2bd0
danielsky <karma0rg4u@gmail.com>
2018-10-19 20:53:12 +0100

    Additional Exercises

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
 $(document).ready(function() {
 
-
 	$("p").click(function() {
-		$("p").css("color","red");
+		$(this).children("a").css("background-color", "yellow");
 	});
-	$("h2").hover(function() {
-		$("h2").css("background-color","lightblue");
-		$(this).css("font-size","24px");
-	});
-	$(".bottom_button").hover(
-		function() {
-			$("body").css("background-color","black");
-			}, function() {
-				$("body").css("background-color","grey");
-			}
-		); 
- 
 
- 	$("#stream1_btn").on("click", function() {			// can use instead $("#stream1_btn").click(function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
-	});
-	$("#stream2_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
-	});
-	$("#stream3_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
-	});
+	
+
+	// $("a").replaceWith("<button>Button</button>");
+	// $("button").addClass("bottom_button makeRed");
+
+	// $("button").addClass("makeRed");
+
+	// $("button")
+	// .mouseenter(function() {
+	// 	$(this).removeClass("makeRed").addClass("makeBorder");
+	// })
+	// .mouseleave(function() {
+	// 	$(this).removeClass("makeBorder").addClass("makeRed")
+	// });
+
+	// $("button").click(function() {
+	// 	$("p").slideToggle("slow");
+	// });
+	
+		
+
+	// $("p").click(function() {
+	// 	$("p").css("color","red");
+	// });
+	// $("h2").hover(function() {
+	// 	$("h2").css("background-color","lightblue");
+	// 	$(this).css("font-size","24px");
+	// });
+	// $(".bottom_button").hover(
+	// 	function() {
+	// 		$("body").css("background-color","black");
+	// 		}, function() {
+	// 			$("body").css("background-color","grey");
+	// 		}
+	// 	); 
+ 	
+	// $("#stream1_btn").on("click", function() {
+	// 	$(".stream1").toggle("fast");
+	// });
+
+
+
+	// $("button")
+	// .mouseenter(function() {
+	// 	$(this).fadeTo("slow", 0.3);
+	// })
+	// .mouseleave(function() {
+	// 	$(this).fadeTo("slow", 1);
+	// });
+
+
+ // 	$("#stream1_btn").on("click", function() {			
+ // 		$(".stream1").removeClass('highlight_stream');
+	// 	$(".stream2").removeClass('highlight_stream');
+	// 	$(".stream3").removeClass('highlight_stream');
+	//   	$(".stream1").addClass('highlight_stream');
+	// });
+	// $("#stream2_btn").on("click", function() {
+	// 	$(".stream1").removeClass('highlight_stream');
+	// 	$(".stream2").removeClass('highlight_stream');
+	// 	$(".stream3").removeClass('highlight_stream');
+	//   	$(".stream2").addClass('highlight_stream');
+	// });
+	// $("#stream3_btn").on("click", function() {
+	// 	$(".stream1").removeClass('highlight_stream');
+	// 	$(".stream2").removeClass('highlight_stream');
+	// 	$(".stream3").removeClass('highlight_stream');
+	//   	$(".stream3").addClass('highlight_stream');
+	// });
 
 
 }); 
